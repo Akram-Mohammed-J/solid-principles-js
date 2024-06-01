e3f13af7083fb5af62749ece019cc9475c2696bf
Apple <coddletechnologies@Coddles-MacBook-Pro.local>
2024-06-01 12:04:44 +0530

    added definition for SRP

--- SingleResponsiblityPrinciple.js
+++ SingleResponsiblityPrinciple.js
@@ -1,3 +1,14 @@
+/**
+ * This means that a class must have only one responsibility.
+
+When a class performs just one task, it contains a small number of methods and
+member variables making them more usable and easier to maintain.
+
+If a class has multiple responsibilities, it becomes harder to understand,
+maintain, and modify and increases the potential for bugs because changes to one
+responsibility could affect the others.
+ */
+
 class UserManager {
   authenticateUser(username, password) {
     //some logic
